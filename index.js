b4f98606eca7555bd7ca3cb2a53bb4beae6a9377
LucianaChamma <lusahd@hotmail.com>
2023-07-04 00:23:29 -0300

    exercicios 1 2 3

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello world!");
+
+// 1)Escreva uma função chamada dobrar que recebe um número como parâmetro e retorna o dobro desse número. Use a sintaxe de função tradicional. 
+function dobroNum(){
+    let num = 4;
+    return (num)*2;
+ }
+ 
+ console.log(dobroNum());   
+
+  
+
+// 2)Reescreva a função dobrar utilizando a sintaxe de função anônima atribuída a uma variável. 
+
+function dobroNum1(num1){
+   
+return(num1)*2;
+}
+    console.log(dobroNum1(6));
+
+
+// 3)Transforme a função dobrar em uma função de seta (arrow function). 
+
+let dobroArrow = (num1) => { return num1 *2 };
+
+ console.log(dobroArrow());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
